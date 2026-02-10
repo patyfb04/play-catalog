de755479de756e8356d9f427b09362596b1b8d5d
Patricia <patyfb04@gmail.com>
2026-02-09 21:33:32 -0500

    Add Play.Catalog files

---
+++ src/Play.Catalog.Service/Dtos/CatalogDtos.cs
@@ -0,0 +1,8 @@
+﻿namespace Play.Catalog.Service.DTOs
+{
+    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+
+    public record CreateItemDto(string Name, string Description, decimal Price);
+
+    public record UpdateItemDto(string Name, string Description, decimal Price);
+}
