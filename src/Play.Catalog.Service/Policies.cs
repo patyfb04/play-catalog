292adf6757d9c7f568c51d99b2c1697ed4321c11
Patricia <patyfb04@gmail.com>
2026-02-10 22:07:36 -0500

    git commit -m "Remove all files listed in .gitignore from tracking"

--- src/Play.Catalog.Service/Policies.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Play.Catalog.Service.Policies
-{
-    public static class Policies
-    {
-        public const string Read = "read_access";
-
-        public const string Write = "write_access";
-    }
-}
